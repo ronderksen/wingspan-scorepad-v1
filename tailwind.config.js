03c2419650156b3490305bed8aef472e155b1d49
ronderksen <ron.derksen@trivento.nl>
2021-07-26 22:46:19 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    "./app/**/*.tsx",
+    "./app/**/*.ts"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
